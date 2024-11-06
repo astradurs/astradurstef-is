3bfcf5842ed65e03e794d89e25ba88a9289917f8
Ástráður Stefánsson <astradur@maul.is>
2024-11-06 17:46:17 +0000

    removed sans fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
 import { Flex, Theme } from "@radix-ui/themes"
 import "@radix-ui/themes/styles.css"
 import type { Metadata } from "next"
-import localFont from "next/font/local"
 import Header from "./components/header"
 import "./globals.css"
 import { Providers } from "./providers"
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-})
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
